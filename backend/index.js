5ab53a8f4fec984fa98fafb869e65a98e2f85d28
FunChosa <gilazieva.00@mail.ru>
2024-10-28 20:30:58 +0300

    APIs for notes added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 mongoose.connect(config.connectionString);
 
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 const express = require("express");
 const cors = require("cors");
@@ -115,6 +116,155 @@ app.post("/login", async (req, res) => {
       .json({ error: true, message: "Invalid email or password" });
   }
 });
+
+// Add Note
+
+app.post("/add-note", authenticationToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+
+    await note.save();
+
+    return res.json({ error: false, note, message: "Note added successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal server error" });
+  }
+});
+
+// Edit Note
+
+app.put("/edit-note/:noteId", authenticationToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body;
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ error: true, message: "No changes provided" });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal server error" });
+  }
+});
+
+// Get all notes
+
+app.get("/get-all-notes", authenticationToken, async (req, res) => {
+  const { user } = req.user;
+
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+
+    return res.json({
+      error: false,
+      notes,
+      message: "Notes fetched successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal server error" });
+  }
+});
+
+// Delete note
+
+app.delete("/delete-note/:noteId", authenticationToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+    await Note.deleteOne({ _id: noteId, userId: user._id });
+
+    return res.json({ error: false, message: "Note deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal server error" });
+  }
+});
+
+// update isPinned
+
+app.put(
+  "/update-note-pinned/:noteId",
+  authenticationToken,
+  async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    try {
+      const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+      if (!note) {
+        return res.status(404).json({ error: true, message: "Note not found" });
+      }
+
+      note.isPinned = isPinned;
+
+      await note.save();
+
+      return res.json({
+        error: false,
+        note,
+        message: "Note updated successfully",
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: true, message: "Internal server error" });
+    }
+  }
+);
 
 app.listen(8000);
 
